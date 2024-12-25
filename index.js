4e14337b83ea60132710788fc2ff79cbe8e790bb
fatma200412 <bd7gq9082@code.edu.az>
2024-12-25 10:39:49 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { createServer } = require("node:http");
+
+const { Server } = require("socket.io");
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+// console.log("salam");
+let arr = [];
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("message", (message) => {
+    console.log(message, socket.id);
+    arr.push(message);
+    io.emit("message", arr);
+  });
+});
+
+console.log("first");
+server.listen(3003, () => {
+  console.log(`Example app listening on port`);
+});
+//////////////////////////////////////
